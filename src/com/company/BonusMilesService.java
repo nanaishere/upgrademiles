60f0e1e1ab4edff4ad543c6e4803ea807c130dc1
nanaishere <naiavasilyeva@gmail.com>
2021-08-20 11:59:50 +0900

    Added files

---
+++ src/com/company/BonusMilesService.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class BonusMilesService {
+     public int calculate(int price) {
+            int totalmiles = price / 20;
+            return totalmiles;
+        }
+    }
+
